2d0e4ad9216b8fe3d0033388977ade29f314da8c
liza4231 <liza4231@gmail.com>
2026-05-23 19:00:43 -0500

    1st attempt

--- server/app.py
+++ server/app.py
@@ -1,1 +1,25 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# Existing car models
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+
+# Default route
+@app.route("/")
+def home():
+    return "Welcome to Flatiron Cars"
+
+# Dynamic model route
+@app.route("/<model>")
+def check_model(model):
+    if model in existing_models:
+        return f"Flatiron {model} is in our fleet!"
+    else:
+        return f"No models called {model} exists in our catalog"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
